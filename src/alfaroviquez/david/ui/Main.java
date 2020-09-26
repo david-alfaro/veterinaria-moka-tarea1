3eb627710d3b29baaa70d8af999850e09343aa49
david-alfaro <dalfarov1@ucenfotec.ac.cr>
2020-09-26 13:38:47 -0600

    se agrego el metodo de registrar y listar usuario

--- src/alfaroviquez/david/ui/Main.java
+++ src/alfaroviquez/david/ui/Main.java
@@ -1,5 +1,6 @@
 package alfaroviquez.david.ui;
 
+import alfaroviquez.david.bl.Usuario;
 import java.io.PrintStream;
 import java.util.Scanner;
 
@@ -7,14 +8,16 @@ public class Main {
 
     private static Scanner input = new Scanner(System.in).useDelimiter("\\n");
     private static PrintStream output = new PrintStream(System.out);
+    private static Usuario[] arregloUsuarios = new Usuario[50];
+    private static int nextUsuario = 1;
 
     public static void main(String[] args) {
         int opcion = 0;
-        do{
+        do {
             MostrarMenu();
             opcion = input.nextInt();
-            procesarOpcion();
-        }while(opcion!=9);
+            procesarOpcion(opcion);
+        } while (opcion != 9);
     }
 
     private static void MostrarMenu() {
@@ -31,7 +34,71 @@ public class Main {
         output.println("Ingrese una opción -> ");
     }
 
-    private static void procesarOpcion() {
-        
+    private static void procesarOpcion(int opcion) {
+        switch (opcion) {
+            case 1:
+                registrarUsuario();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                listarUsuarios();
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            default:
+                output.println("Opicón inválida! ");
+        }
+    }
+
+    private static void registrarUsuario() {
+        output.println("Nombre: ");
+        String Nombre = input.next();
+        output.println("Primer Apellido: ");
+        String Apellido1 = input.next();
+        output.println("Segundo Apellido: ");
+        String Apellido2 = input.next();
+        output.println("Número de cédula: ");
+        int Cedula = input.nextInt();
+        output.println("Número de teléfono: ");
+        int Telefono = input.nextInt();
+        output.println("Direccion: ");
+        String Direccion = input.next();
+        output.println("Estado (Activo/Inactivo): ");
+        String Estado = input.next();
+        output.println("Rol: ");
+        String Rol = input.next();
+        Usuario nuevoUsuario = new Usuario(Nombre, Apellido1, Apellido2, Cedula, Telefono, Direccion, Estado, Rol);
+       
+        for(int i=0; i<arregloUsuarios.length;i++){
+            if(arregloUsuarios[i]!=null){
+                if(arregloUsuarios[i].getCedula()== nuevoUsuario.getCedula()){
+                    output.println("El usuario ya existe en el sistema");
+                }
+            }else{
+                arregloUsuarios[i]=nuevoUsuario;
+                break;
+                
+            }
+        }
+        output.println("Usuario registrado de forma exitosa ");
+      
+
+    }
+
+    private static void listarUsuarios() {
+        for(int i=0;i<arregloUsuarios.length;i++){
+            output.println("#"+(i+1)+": "+arregloUsuarios[i]);
+        }
     }
 }
