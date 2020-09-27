38fb321241b7bdff10f7c876557f059335ba6e83
david-alfaro <dalfarov1@ucenfotec.ac.cr>
2020-09-26 19:08:51 -0600

    se corrigio la clase usuario y se agrego la clase mascota

--- src/alfaroviquez/david/ui/Main.java
+++ src/alfaroviquez/david/ui/Main.java
@@ -1,5 +1,6 @@
 package alfaroviquez.david.ui;
 
+import alfaroviquez.david.bl.Mascota;
 import alfaroviquez.david.bl.Usuario;
 import java.io.PrintStream;
 import java.util.Scanner;
@@ -9,7 +10,8 @@ public class Main {
     private static Scanner input = new Scanner(System.in).useDelimiter("\\n");
     private static PrintStream output = new PrintStream(System.out);
     private static Usuario[] arregloUsuarios = new Usuario[50];
-    private static int nextUsuario = 1;
+    private static Mascota[] arregloMascotas = new Mascota[50];
+
 
     public static void main(String[] args) {
         int opcion = 0;
@@ -40,6 +42,7 @@ public class Main {
                 registrarUsuario();
                 break;
             case 2:
+                registrarMascota();
                 break;
             case 3:
                 break;
@@ -49,6 +52,7 @@ public class Main {
                 listarUsuarios();
                 break;
             case 6:
+                listarMascota();
                 break;
             case 7:
                 break;
@@ -79,26 +83,71 @@ public class Main {
         output.println("Rol: ");
         String Rol = input.next();
         Usuario nuevoUsuario = new Usuario(Nombre, Apellido1, Apellido2, Cedula, Telefono, Direccion, Estado, Rol);
-       
-        for(int i=0; i<arregloUsuarios.length;i++){
-            if(arregloUsuarios[i]!=null){
-                if(arregloUsuarios[i].getCedula()== nuevoUsuario.getCedula()){
+
+        for (int i = 0; i < arregloUsuarios.length; i++) {
+            if (arregloUsuarios[i] != null) {
+                if (arregloUsuarios[i].getCedula() == nuevoUsuario.getCedula()) {
                     output.println("El usuario ya existe en el sistema");
+                    arregloUsuarios[i]=null;
+                    
                 }
-            }else{
-                arregloUsuarios[i]=nuevoUsuario;
+            } else {
+                arregloUsuarios[i] = nuevoUsuario;
+                output.println("Usuario registrado de forma exitosa ");
                 break;
-                
+
             }
         }
-        output.println("Usuario registrado de forma exitosa ");
-      
+        
 
     }
 
     private static void listarUsuarios() {
-        for(int i=0;i<arregloUsuarios.length;i++){
-            output.println("#"+(i+1)+": "+arregloUsuarios[i]);
+        for (int i = 0; i < arregloUsuarios.length; i++) {
+            output.println("#" + (i + 1) + ": " + arregloUsuarios[i]);
+        }
+    }
+
+    private static void registrarMascota() {
+        output.println("Nombre de la mascota: ");
+        String NombreMascota = input.next();
+        output.println("Nombre del dueño: ");
+        String NombreDuenno = input.next();
+        output.println("Primer apellido: ");
+        String Apellido1Duenno = input.next();
+        output.println("Segundo apellido: ");
+        String Apellido2Duenno = input.next();
+        output.println("Cedula: ");
+        int CedulaDuenno = input.nextInt();
+        output.println("Telefono: ");
+        int TelefonoDuenno = input.nextInt();
+        output.println("Direccion: ");
+        String DireccionDuenno = input.next();
+        output.println("Foto de la Mascota: ");
+        String FotoMascota = input.next();
+        output.println("Observaciones de la mascota: ");
+        String Observaciones = input.next();
+        output.println("Ranking de la mascota: ");
+        int Ranking = input.nextInt();
+        Mascota nuevaMascota = new Mascota(NombreMascota, NombreDuenno, Apellido1Duenno, Apellido2Duenno, CedulaDuenno, TelefonoDuenno, DireccionDuenno, FotoMascota, Observaciones, Ranking);
+        for(int i=0; i<arregloMascotas.length;i++){
+            if(arregloMascotas[i]!=null){
+                if(arregloMascotas[i].getNombreMascota().equals(nuevaMascota.getNombreMascota())){
+                    output.println("La mascota ya existe en el sistema");
+                    arregloMascotas[i]=null;
+                }
+            }else{
+                arregloMascotas[i]=nuevaMascota;
+                output.println("Se agrego la mascota de forma exitosa!");
+                break;
+            }
+        }
+        
+    }
+
+    private static void listarMascota() {
+        for(int i=0;i<arregloMascotas.length;i++){
+            output.println("#"+(i+1)+arregloMascotas[i]);
         }
     }
 }
